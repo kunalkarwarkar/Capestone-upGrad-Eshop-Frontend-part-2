03a5bc5b194885629f2581d30ae31e40667814fb
kunalkarwarkar <karwarkarkunal@gmail.com>
2022-07-23 21:56:22 +0530

    product statement done

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
 //import reportWebVitals from './reportWebVitals';
+import Product from './components/product';
+import Order from './components/createOrder';
 import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-   document.getElementById('root')
-   );
-   
+ReactDOM.render(<Order/>,document.getElementById('root')
+);
